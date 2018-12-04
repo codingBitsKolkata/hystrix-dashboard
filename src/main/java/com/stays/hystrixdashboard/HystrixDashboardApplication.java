3c8c2df4c9d3bdd26a852e2f2566ef03b2562997
krishanu das <krishanucoder.das@gmail.com>
2018-12-04 17:42:08 +0530

    updated hystrix properties

--- src/main/java/com/stays/hystrixdashboard/HystrixDashboardApplication.java
+++ src/main/java/com/stays/hystrixdashboard/HystrixDashboardApplication.java
@@ -2,12 +2,14 @@ package com.stays.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableHystrix
 @EnableHystrixDashboard
+@EnableDiscoveryClient
 public class HystrixDashboardApplication {
 
 	public static void main(String[] args) {
