fb93f53c7b3f692cdbac62da94491e295fad03ee
Avirup <avirup.pal@gmail.com>
2019-02-02 22:00:32 +0530

    Update HystrixDashboardApplication.java

--- src/main/java/com/orastays/hystrixdashboard/HystrixDashboardApplication.java
+++ src/main/java/com/orastays/hystrixdashboard/HystrixDashboardApplication.java
@@ -1,4 +1,4 @@
-package com.stays.hystrixdashboard;
+package com.orastays.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
